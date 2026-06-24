0dd9815509c2c9ee8f8e16f431e451b931baf1e2
Akshith23 <gajawadaakshith2006@gmail.com>
2026-06-24 19:29:35 +0530

    Add Docker deployment files

--- crystext/backend/app.py
+++ crystext/backend/app.py
@@ -65,4 +65,12 @@ def download_cif():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    import os
+
+    port = int(os.environ.get("PORT", 7860))
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
